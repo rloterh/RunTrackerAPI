4ecde1f3dc1b5e8a5084ca140f64872f7c7e658e
rloterh <rloterh@gmail.com>
2021-12-13 05:45:12 +0000

    Clean syntax

--- spec/requests/api/v1/feeds_spec.rb
+++ spec/requests/api/v1/feeds_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Feeds", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/feeds/index"
+RSpec.describe 'Api::V1::Feeds', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/api/v1/feeds/index'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
