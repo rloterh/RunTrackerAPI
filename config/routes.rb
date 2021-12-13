d3cdd1f76fb07e2ed6b3a34a2aabbb8c101d8e62
rloterh <rloterh@gmail.com>
2021-12-13 05:39:40 +0000

    Update Feed endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
-      get 'feeds/index'
+    namespace :v1, defaults: { format: 'json' } do
+      get 'feeds', to: 'feeds#index'
     end
+
+    root "feeds#index"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
