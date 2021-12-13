280c7df1fccab1053c4ca4206bff74ba7a33a0c4
rloterh <rloterh@gmail.com>
2021-12-13 05:36:54 +0000

    Update index action to render json

--- app/controllers/api/V1/feeds_controller.rb
+++ app/controllers/api/V1/feeds_controller.rb
@@ -1,4 +1,8 @@
 class Api::V1::FeedsController < ApplicationController
   def index
+    random = Random.new
+    limit = Feed.all.length
+    @feed = Feed.find(random.rand(1..limit)).message
+    render json: @feed, status: :ok
   end
 end
