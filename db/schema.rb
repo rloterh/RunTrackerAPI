de18fba5c0242977d98a06b9c46315385934a0e4
rloterh <rloterh@gmail.com>
2021-12-18 07:21:23 +0000

    Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_163035) do
+ActiveRecord::Schema.define(version: 2021_12_18_061344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,34 @@ ActiveRecord::Schema.define(version: 2021_12_12_163035) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "sprints", force: :cascade do |t|
+    t.integer "target"
+    t.integer "hours"
+    t.integer "minutes"
+    t.integer "seconds"
+    t.integer "distance"
+    t.text "remarks"
+    t.decimal "effort"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "trackers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "sprint_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sprint_id"], name: "index_trackers_on_sprint_id"
+    t.index ["user_id"], name: "index_trackers_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "trackers", "sprints"
+  add_foreign_key "trackers", "users"
 end
