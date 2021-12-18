44f48f81d9e53abb22e7593e738d9c1060b7bc5f
rloterh <rloterh@gmail.com>
2021-12-18 07:13:22 +0000

    Update enpoint routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1, defaults: { format: 'json' } do
-      get 'feeds', to: 'feeds#index'
+    namespace :v1 do
+      resources :sprints, only: [:index, :show, :create, :update]
+      resource :users, only: [:index, :create]
+      post '/login', to: 'users#login'
+      get '/auto_login', to: 'users#auto_login'
     end
-
-    root "feeds#index"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
